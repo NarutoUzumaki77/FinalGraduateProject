9864380a6115fa4a3d6ca02e786ade3d2b4c9076
NarutoUzumaki77 <kellyemeka7@gmail.com>
2017-05-12 11:15:21 -0400

    Generate and position NPC

---
+++ Assets/Script/NonPlayableEntities.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NonPlayableEntities : MonoBehaviour {
+    
+    public GameObject[] entity;
+
+    private int npeSize;
+
+	// Use this for initialization
+	void Start () {
+        npeSize = entity.Length * 2;
+
+        for (int i = 0; i < npeSize; i++){
+            string position = DetermineEntityDropPosition();
+            string[] posArray = position.Split('|');
+            print(position); 
+            float x = float.Parse(posArray[0]);
+            float y = float.Parse(posArray[1]);
+            Instantiate(entity[0], new Vector3(x, y, 0f), Quaternion.identity);
+        }
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        //print("Collided with " + collision.gameObject.tag);
+    }
+
+    private string DetermineEntityDropPosition() {
+
+        bool posFoundinList = false;
+        string position = null;
+
+        do
+        {
+            posFoundinList = false;
+			int x = Mathf.RoundToInt(Random.Range(1, 9));
+			int y = Mathf.RoundToInt(Random.Range(1, 5));
+            position = x + "|" + y;
+
+			foreach (string item in PCG_MazeBricks.mazePositions) {
+                if (item.Equals(position)){
+                    posFoundinList = true;   
+                }
+			}
+        } while (posFoundinList);
+
+        return position;
+
+    }
+}
--- Assets/Script/PCG_MazeBricks.cs
+++ Assets/Script/PCG_MazeBricks.cs
@@ -1,18 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PCG_MazeBricks : MonoBehaviour {
 
 	public GameObject brick;
 	public int colheight;
 	public int rowheight;
+    public static List<string> mazePositions = new List<string>();
 
 	private float xOffset = 0.97f;
 	private float yOffset = 0.17f;
-
 	private int [,] worldMap;
 
-	// Use this for initialization
 	void Start () {
 
 		worldMap = new int[colheight, rowheight];
@@ -20,19 +20,17 @@ public class PCG_MazeBricks : MonoBehaviour {
 		for (int i = 0; i < colheight; i++) {
 			for (int j = 0; j < rowheight; j++){
 				worldMap[i,j] = Mathf.RoundToInt(Random.Range(0,5));
-				print ("["+i+","+j+"] = "+ worldMap[i,j]);
 
 				if (worldMap [i,j]>=4) {
-					GameObject t = (GameObject)(Instantiate (brick, new Vector3 (j+xOffset, i+yOffset+1f, 0),
+                    float x = j + xOffset;
+                    float y = i + yOffset + 1f;
+					GameObject t = (GameObject)(Instantiate (brick, new Vector3 (x, y, 0),
 											Quaternion.identity));
 					t.transform.parent = this.transform;
+                    string position = Mathf.RoundToInt(x) + "|" + Mathf.RoundToInt(y);
+                    mazePositions.Add(position);
 				}
 			}
 		}
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 }
